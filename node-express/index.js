f3c8d770f6aef3cfbdff0c748f9e2ddaa4066373
MattEastman <designeastman@gmail.com>
2019-02-25 23:45:47 -0800

    express-gen api

--- node-express/index.js
+++ node-express/index.js
@@ -3,25 +3,26 @@ const http= require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 const hostname = 'localhost';
-const port = 3003;
+const port = 3030;
 
 const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 
-
-
-
 app.use('/dishes',dishRouter);
+app.use('/promotions',promoRouter);
+app.use('/leaders',leaderRouter);
 app.use(express.static(__dirname+'/public'));
 
 app.use((req, res, next) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+  res.end('<html><body><h1>Serving from index</h1></body></html>');
 
 });
 
